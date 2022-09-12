b309212af6c18326691db6bbb9e3891f33ca567c
Cauan <cauanbarros14@gmail.com>
2022-09-12 09:56:09 -0300

    req 12 quase la

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/* var botao = document.querySelector("button")
-botao.addEventListener("click", gerar_cor)
-
-
- function gerar_cor() {
-let r = parseInt(Math.random() * 255),
-    g = parseInt(Math.random() * 255),
-    b = parseInt(Math.random() * 255)
-
-   return 'rgb('+r+', '+g+', '+b+')'
-}
-
-function alerta(){
-    alert(gerar_cor)
-}
-
-function cores() {
-    let seleciona = document.getElementsByClassName("color");
-
-    for(i = 0; i < seleciona.length; i += 1) {
-        let man = seleciona[i];
-        man = gerar_cor
-    }
-}
-
-alerta() */
-
 const palet = document.getElementsByClassName("nome");
 
 
@@ -42,118 +15,124 @@ const palet = document.getElementsByClassName("nome");
             let recebe = palet[i];
             recebe.style.background = coresPaleta();
         
-            /* recebe.addEventListener("select", function() {
-                let pegaCor = document.getElementsByClassName("color nome");
-                for(let i = 0; i < pegaCor.length; i += 1) {
-               pegaCor[i]
-            }
-                } ) */
-            }
-
-            //localStorage.setItem("colorPalette")
         }
-
-        function botao() {
-            var oBotao = document.getElementById("button-random-color");
-            oBotao.addEventListener("click", paleta);
-
-           // localStorage.setItem("colorPalette")
-            
-        }
-        botao() 
-    
-        function salveLocal() {
-            let vazio = []
-            for(let i = 0; i < palet.length; i += 1) {
-                vazio.push(palet[i].style.backgroundColor)
-              //  console.log(palet[i].style.backgroundColor)
-            }
-            let abc = JSON.stringify(vazio)
-            // console.log(JSON.stringify(vazio))
-             localStorage.setItem("colorPalette", abc)
-            }
+    }
            
-            function verificaStorage() {
-                if(localStorage.length === 0) {
-                    return true
+    function botao() {
+        var oBotao = document.getElementById("button-random-color");
+        oBotao.addEventListener("click", paleta);
+        
+    }
+    botao() 
+
+    function salveLocal() {
+        let vazio = []
+        for(let i = 0; i < palet.length; i += 1) {
+            vazio.push(palet[i].style.backgroundColor)
+          
+        }
+        let abc = JSON.stringify(vazio)
+         localStorage.setItem("colorPalette", abc)
+        }
+       
+        function verificaStorage() {
+            if(localStorage.length === 0) {
+                return true
+            }
+            return false
+           }
+        
+           if(verificaStorage()) {
+            paleta() 
+            salveLocal()
+           }
+           else {
+            let recupera = JSON.parse(localStorage.getItem("colorPalette"))
+            console.log(recupera)
+             for(let i = 0; i < palet.length; i += 1) {
+                console.log(palet[i], recupera[i])
+                palet[i].style.backgroundColor = recupera[i]
+             }
+           }
+
+           function seleciona() {
+               let minhasCores = document.querySelectorAll(".color nome")
+               for(let i = 0; i < minhasCores.length; i += 1) {
+                   minhasCores[i].addEventListener("mousedown", function(){
+                       document.style.backgroundColor = paleta[i]
+                   })
+               }
+           }
+         
+           let chamaClass = document.querySelector(".selected");
+           let color = document.querySelectorAll(".color");
+
+           function queRemove(event)  {
+               
+                for(let i = 0; i < color.length; i += 1) {
+                   color[i].classList.remove("selected");
+                   
                 }
-                return false
-               }
-             //  console.log(verificaStorage())
-               if(verificaStorage()) {
-                paleta() 
-                salveLocal()
-               }
-               else {
-                let recupera = JSON.parse(localStorage.getItem("colorPalette"))
-                console.log(recupera)
-                 for(let i = 0; i < palet.length; i += 1) {
-                    console.log(palet[i], recupera[i])
-                    palet[i].style.backgroundColor = recupera[i]
-                 }
-               }
-
-               function seleciona() {
-                   let minhasCores = document.querySelectorAll(".color nome")
-                   for(let i = 0; i < minhasCores.length; i += 1) {
-                       minhasCores[i].addEventListener("mousedown", function(){
-                           document.style.backgroundColor = paleta[i]
-                       })
-                   }
-               }
-             
-               let chamaClass = document.querySelector(".selected");
-               let color = document.querySelectorAll(".color");
-
-               function queRemove(event)  {
-                   //let color = document.querySelectorAll(".color");
-               // console.log(color)
-                    for(let i = 0; i < color.length; i += 1) {
-                       color[i].classList.remove("selected");
-                       //event.target.classList.add("selected");
-                    }
-                    event.target.classList.add("selected");
-
-                    }
-                function adPixel() {
-                   let pegaPixel = document.querySelectorAll(".color")
-                    for(let i = 0; i < pegaPixel.length; i += 1) {
-                        pegaPixel[i].addEventListener("click", queRemove)
-                    }
+                event.target.classList.add("selected");
 
                 }
+            function adPixel() {
+               let pegaPixel = document.querySelectorAll(".color")
+                for(let i = 0; i < pegaPixel.length; i += 1) {
+                    pegaPixel[i].addEventListener("click", queRemove)
+                }
 
-                function colorPixel() {
-                    let meusPixeis = document.querySelectorAll(".pixel");
-                   // let asPalet = document.querySelector("#color-palette");
-                    //let guardaCor = document.querySelector(".selected")
+            }
+
+            function colorPixel() {
+                let meusPixeis = document.querySelectorAll(".pixel");
+                for(let i = 0; i < meusPixeis.length; i += 1) {
+                 meusPixeis[i].addEventListener("click", function(event) {
+                    let guardaCor = document.querySelector(".selected")
+                    console.log(guardaCor.style.backgroundColor);
+                   event.target.style.backgroundColor = guardaCor.style.backgroundColor
+                  
+                 })
+                }
+
+            }
+
+            function clear() {
+                let meusPixeis = document.querySelectorAll(".pixel");
+                let botaoLimpa = document.querySelector("#clear-board");
+
+                botaoLimpa.addEventListener("click", function() {
+                    for(let i = 0; i < meusPixeis.length; i += 1){
+                        meusPixeis[i].style.backgroundColor = "white";
+                    }
+                })
+                
+
+            }
+
+            function salvaDesenhoPaleta() {
+                let meusPixeis = document.querySelectorAll(".pixel");
+                let vazio = []; 
+
+                /* meusPixeis.addEventListener("click", function() { */
                     for(let i = 0; i < meusPixeis.length; i += 1) {
-                     meusPixeis[i].addEventListener("click", function(event) {
-                        let guardaCor = document.querySelector(".selected")
-                        console.log(guardaCor.style.backgroundColor);
-                       event.target.style.backgroundColor = guardaCor.style.backgroundColor
-                      /*   for(let i = 0; i < asPalet.length; i += 1) {
-                            if(asPalet[i].style.backgroundColor === guardaCor.style.backgroundColor) {
-                                meusPixeis[i].style.backgroundColor = guardaCor.style.backgroundColor
-                            }
-                        } */
-                     })
+                        vazio.push(meusPixeis[i].style.backgroundColor)
                     }
 
-                }
-
-                function clear() {
-                    let meusPixeis = document.querySelectorAll(".pixel");
-                    let botaoLimpa = document.querySelector("#clear-board");
-
-                    botaoLimpa.addEventListener("click", function() {
-                        for(let i = 0; i < meusPixeis.length; i += 1){
-                            meusPixeis[i].style.backgroundColor = "white";
-                        }
-                    })
+                    let reVazio = JSON.stringify(vazio);
+                    localStorage.setItem("pixelBoard", reVazio)
                     
+                    
+                }//)
+           // }
+            
+            
+            /*  if(meusPixeis[i] === guardaCor.style.backgroundColor) {
+                 meusPixeis[i] = localStorage.setItem("pixelBoard",meusPixeis[i] )
+             } else {
+                 localStorage.getItem("pixeBoard")
+             } */
 
-                }
 
                 
 
@@ -166,6 +145,7 @@ const palet = document.getElementsByClassName("nome");
 adPixel(); 
 colorPixel();
 clear();
+salvaDesenhoPaleta();
 
            
             
