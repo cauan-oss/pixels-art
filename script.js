56185dd3efdc6e0740295de0bd56fb4fd12cb976
Cauan <cauanbarros14@gmail.com>
2022-09-12 19:57:07 -0300

    REQ 5 CONCLUIDOOOO

--- script.js
+++ script.js
@@ -5,24 +5,40 @@ const palet = document.getElementsByClassName("nome");
     let r = Math.floor(Math.random()*255),
         g = Math.floor(Math.random()*255),
         b = Math.floor(Math.random()*255);
-        let minhasCores = `rgb(${r}, ${g}, ${b})`
+        var minhasCores = `rgb(${r}, ${g}, ${b})`
         return minhasCores
  }
 
     function paleta() {
         const palet = document.getElementsByClassName("color");
+        let arrayVazio = [];
         for(let i = 1; i < palet.length; i += 1){
             let recebe = palet[i];
-            recebe.style.background = coresPaleta();
-        
+            //let arrayVazio = [];
+            const x = coresPaleta();
+            recebe.style.background = x;
+            arrayVazio.push(x);
+            //localStorage.setItem("colorPalette", JSON.stringify(arrayVazio))
         }
+
+        localStorage.setItem("colorPalette", JSON.stringify(arrayVazio))
+
+        //localStorage.setItem("colorPalette", JSON.stringify(arrayVazio))
+        
+         
+       // let receb
     }
+    
+      
+    
+
            
     function botao() {
         var oBotao = document.getElementById("button-random-color");
         oBotao.addEventListener("click", paleta);
         
-    }
+        }
+
     botao() 
 
     function salveLocal() {
@@ -36,24 +52,30 @@ const palet = document.getElementsByClassName("nome");
         }
        
         function verificaStorage() {
-            if(localStorage.length === 0) {
+            var meuLocal = JSON.parse(localStorage.getItem("colorPalette"))
+            console.log(meuLocal)
+            if(meuLocal === null) {
                 return true
             }
             return false
            }
+
+           window.onload = function queVerifica() {
+            if(verificaStorage()) {
+                console.log("entrei")
+                paleta() 
+                salveLocal()
+               }
+               else {
+                let recupera = JSON.parse(localStorage.getItem("colorPalette"))
+                //console.log(recupera)
+                 for(let i = 0; i < palet.length; i += 1) {
+                    console.log(palet[i], recupera[i])
+                    palet[i].style.backgroundColor = recupera[i]
+                 }
+               }
+           }
         
-           if(verificaStorage()) {
-            paleta() 
-            salveLocal()
-           }
-           else {
-            let recupera = JSON.parse(localStorage.getItem("colorPalette"))
-            console.log(recupera)
-             for(let i = 0; i < palet.length; i += 1) {
-                console.log(palet[i], recupera[i])
-                palet[i].style.backgroundColor = recupera[i]
-             }
-           }
 
            function seleciona() {
                let minhasCores = document.querySelectorAll(".color nome")
@@ -110,28 +132,44 @@ const palet = document.getElementsByClassName("nome");
 
             }
 
-            function salvaDesenhoPaleta() {
+             /*  function salvaDesenhoPaleta() {
                 let meusPixeis = document.querySelectorAll(".pixel");
                 let vazio = []; 
-
-                /* meusPixeis.addEventListener("click", function() { */
-                    for(let i = 0; i < meusPixeis.length; i += 1) {
+                 meusPixeis.addEventListener("click", function() { 
+                     for(let i = 0; i < meusPixeis.length; i += 1) {
                         vazio.push(meusPixeis[i].style.backgroundColor)
                     }
-
                     let reVazio = JSON.stringify(vazio);
-                    localStorage.setItem("pixelBoard", reVazio)
+                    localStorage.setItem("pixelBoard", reVazio);
+                })
+
+            } */
+ 
+               /*  function verificaStoragePixel(){
+                    if(localStorage.length === nu) {
+                       return true 
+                    }
+                    return false
+                }
+
+                let meusPixeis = document.querySelectorAll(".pixel")
+
+                    if(verificaStoragePixel()) {
+                        salvaDesenhoPaleta()
+                    }
+                    else {
+                        let recupera = JSON.parse(localStorage.getItem(".pixel"));
+                        console.log(recupera);
+                        for(let i = 0; i < meusPixeis.length; i += 1) {
+                            console.log(meusPixeis[i], recupera[i]);
+                            meusPixeis[i].style.backgroundColor = recupera[i];
+                        }
+                    }
+
+ */
+
                     
                     
-                }//)
-           // }
-            
-            
-            /*  if(meusPixeis[i] === guardaCor.style.backgroundColor) {
-                 meusPixeis[i] = localStorage.setItem("pixelBoard",meusPixeis[i] )
-             } else {
-                 localStorage.getItem("pixeBoard")
-             } */
 
 
                 
@@ -145,7 +183,9 @@ const palet = document.getElementsByClassName("nome");
 adPixel(); 
 colorPixel();
 clear();
-salvaDesenhoPaleta();
+//salvaDesenhoPaleta();
+//queVerifica();
+
 
            
             
